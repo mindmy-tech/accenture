97b577c1fe7821f828b06bbd6db1639bf5acc34b
MrMomoLegend <mokithm14@gmail.com>
2023-08-09 22:21:35 +0530

    renamed

--- program.py
+++ program.py
@@ -3,7 +3,7 @@ import os
 cwd=os.getcwd()
 cwd=cwd.removesuffix('pragram.py')
 # Path to the image containing known faces (reference image)
-reference_image_path = rf"{cwd}\accenture\Dataset\WhatsApp Image 2023-08-06 at 8.00.48 PM.jpeg"
+reference_image_path = rf"{cwd}\accenture\Dataset\000"
 
 directory_list = os.listdir(rf"{cwd}\accenture\Dataset\mokith\all images")
 no_of_files = 0
@@ -18,9 +18,9 @@ for path in directory_list:
 # Print the result
     if result["verified"]:
         no_of_verified += 1
-        print("The target image contains the same person as the reference image.")
-        print("Facial distance:", result["distance"])
+       # print("The target image contains the same person as the reference image.")
+       # print("Facial distance:", result["distance"])
     else:
-        print("The target image does not contain the same person as the reference image.")
+        #print("The target image does not contain the same person as the reference image.")
         print("Facial distance:", result["distance"])
 print(no_of_verified,'are matched, out of',no_of_files,'files')
