b6ed9103af170d9017b3091d623cac46de1e72da
MrMomoLegend <mokithm14@gmail.com>
2023-08-09 19:52:51 +0530

    checking for invalid image

--- program.py
+++ program.py
@@ -3,19 +3,21 @@ import os
 cwd=os.getcwd()
 cwd=cwd.removesuffix('pragram.py')
 # Path to the image containing known faces (reference image)
-reference_image_path = rf"{cwd}\accenture\Dataset\Screenshot 2023-08-06 194422.png"
+reference_image_path = rf"{cwd}\accenture\Dataset\WhatsApp Image 2023-08-06 at 8.00.48 PM.jpeg"
 
-# Path to the image you want to recognize (target image)
-target_image_path = rf"{cwd}\accenture\Dataset\IMG_20230608_160345.jpg"
-
-# Perform face recognition
-result = DeepFace.verify(reference_image_path, target_image_path)
+directory_list = os.listdir(rf"{cwd}\accenture\Dataset\mokith")
+for path in directory_list:
+    # Path to the image you want to recognize (target image)
+    target_image_path = rf"{cwd}\accenture\Dataset\mokith\{path}"
+    # Perform face recognition
+    result = DeepFace.verify(reference_image_path, target_image_path, enforce_detection = False)
 
 # Print the result
-if result["verified"]:
-    print("The target image contains the same person as the reference image.")
-    print("Facial distance:", result["distance"])
-else:
-    print("The target image does not contain the same person as the reference image.")
-    print("Facial distance:", result["distance"])
-recognition = DeepFace.find(img_path = reference_image_path, db_path = rf"{cwd}/accenture/dataset/mokith")
+    if result["verified"]:
+        pass
+       # print("The target image contains the same person as the reference image.")
+       # print("Facial distance:", result["distance"])
+    else:
+        os.remove(target_image_path)
+       # print("The target image does not contain the same person as the reference image.")
+       # print("Facial distance:", result["distance"])
