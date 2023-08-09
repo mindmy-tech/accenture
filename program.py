bd19f81b45fe5ddf2a173ebf0a9b50066b899d10
MrMomoLegend <mokithm14@gmail.com>
2023-08-09 19:20:53 +0530

    tested with the Dataset

--- program.py
+++ program.py
@@ -6,7 +6,7 @@ cwd=cwd.removesuffix('pragram.py')
 reference_image_path = rf"{cwd}\accenture\Dataset\Screenshot 2023-08-06 194422.png"
 
 # Path to the image you want to recognize (target image)
-target_image_path = rf"{cwd}\accenture\Dataset\WhatsApp Image 2023-08-06 at 8.00.49 PM (1).jpeg"
+target_image_path = rf"{cwd}\accenture\Dataset\IMG_20230608_160345.jpg"
 
 # Perform face recognition
 result = DeepFace.verify(reference_image_path, target_image_path)
@@ -18,3 +18,4 @@ if result["verified"]:
 else:
     print("The target image does not contain the same person as the reference image.")
     print("Facial distance:", result["distance"])
+recognition = DeepFace.find(img_path = reference_image_path, db_path = rf"{cwd}/accenture/dataset/mokith")
