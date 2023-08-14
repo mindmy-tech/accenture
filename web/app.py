ae77e115a6104932a5faece4c07fabfcb3bb9299
Sabari <education@mindmy.tech>
2023-08-14 12:30:42 +0530

    web

---
+++ web/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+import cv2
+import base64
+from threading import Thread
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
+
+def stream_video():
+    cap = cv2.VideoCapture(0)  # Use the appropriate camera index or video file
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        _, buffer = cv2.imencode('.jpg', frame)
+        frame_data = base64.b64encode(buffer).decode('utf-8')
+
+        socketio.emit('video_frame', frame_data, broadcast=True)
+
+if __name__ == '__main__':
+    video_thread = Thread(target=stream_video)
+    video_thread.start()
+    socketio.run(app, host='0.0.0.0', port=5000)
